d49d7255854a9120603e7a42386164262eb11942
Jayesh Gujar <121622212+JayeshGujar327@users.noreply.github.com>
2026-08-17 20:50:51 +0530

    Fix GitHub Pages routing and Vite base path

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import { createRoot } from "react-dom/client";
 import React from "react";
-import App from "./App.jsx";
+import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import App from "./App.jsx";
 
-createRoot(document.getElementById("root")).render(
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+        <BrowserRouter basename="/NEXORA">
+            <App />
+        </BrowserRouter>
+    </React.StrictMode>
 );
